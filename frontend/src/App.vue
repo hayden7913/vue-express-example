7b3513af179a44a3c1cd93f7823e47cffc92bd33
hayden7913 <hayden321@gmail.com>
2019-01-30 17:02:41 -0800

    First commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+    <SwitchControl />
+  </div>
+</template>
+
+<script>
+import SwitchControl from './components/SwitchControl'
+
+export default {
+  name: 'App',
+  components: {
+    SwitchControl
+  },
+}
+</script>
+
+<style>
+.app-container {
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/SwitchControl.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="switch-control">
+    <div class="switch-control-col switch-control-col-left">
+      <div class="switch-control-label">Power</div>
+    </div>
+    <div class="switch-control-col switch-control-col-right">
+      <div class="switch control-message">OFF</div>
+      <div class="switch-control-switch">switch</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SwitchControl',
+};
+
+</script>
+
+<style>
+.switch-control {
+  margin-left: 30%;
+  display: flex;
+  width: 400px;
+  padding: 10px;
+  /* border: 1px solid #000; */
+}
+
+.switch-control div {
+  /* border: 1px solid gray; */
+}
+
+.switch-control-col-right {
+  display: flex;
+  flex-direction: row;
+  margin-left: auto;
+}
+
+.switch-control-switch {
+  margin-left: 10px;
+}
+</style>
