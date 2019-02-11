dc736fe00793c55d2cd57e08102a00fc5eb80725
hayden7913 <hayden321@gmail.com>
2019-02-10 23:00:04 -0800

    Adds gitignore

--- backend/dosis.js
+++ backend/dosis.js
@@ -2,11 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const environmentState = require('./data/mockDataEnvironment');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
-const port = 3000;
 
 app.use(cors());
 app.use(express.static('dist'));
 
 app.get('/environment', (req, res) => res.json(environmentState));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
